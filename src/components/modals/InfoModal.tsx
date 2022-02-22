a001a52389fac63291c4f08dc94dbc38dae1fe0d
David Imre <envagyok@idavid.hu>
2022-02-22 14:32:45 +0100

    Fixed modal

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -157,6 +157,7 @@ export const InfoModal = ({
         </ul>
       </div>
       <button
+        tabIndex={-1}
         type="button"
         className="mx-auto mt-2 flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 select-none"
         onClick={() => {
--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -257,6 +257,7 @@ export const StatsModal = ({
                           <>
                             <div className="ml-2 mt-5 sm:mt-6">
                               <button
+                                tabIndex={-1}
                                 type="button"
                                 className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
                                 onClick={handleShareClick}
